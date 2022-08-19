aef2c98e49310e2b943ab7685198bd944fd1d13e
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-08-20 01:31:49 +0400

    created js questions

---
+++ index.js
@@ -0,0 +1,32 @@
+const Questions = [{
+        id: 0,
+        q: "What is capital of India?",
+        a: [{ text: "gandhinagar", isCorrect: false },
+            { text: "Surat", isCorrect: false },
+            { text: "Delhi", isCorrect: true },
+            { text: "mumbai", isCorrect: false }
+        ]
+
+    },
+    {
+        id: 1,
+        q: "What is the capital of Thailand?",
+        a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+            { text: "phuket", isCorrect: false },
+            { text: "Ayutthaya", isCorrect: false },
+            { text: "Bangkok", isCorrect: true }
+        ]
+
+    },
+    {
+        id: 2,
+        q: "What is the capital of Gujarat",
+        a: [{ text: "surat", isCorrect: false },
+            { text: "vadodara", isCorrect: false },
+            { text: "gandhinagar", isCorrect: true },
+            { text: "rajkot", isCorrect: false }
+        ]
+
+    }
+
+]
